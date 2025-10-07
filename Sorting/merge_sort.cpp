2be113ec1779a97532d726af4caa689686f2ac85
RohanBisht33 <bishtrohan33@gmail.com>
2025-10-07 23:50:15 +0530

    merge sort in c++

---
+++ Sorting/merge_sort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums, int low, int mid, int high){
+        vector<int> temp;
+        int left = low;
+        int right = mid+1;
+        while(left <= mid && right <= high){
+            if(nums[left] <= nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+        while(left <= mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right <= high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
+    }
+    void merge_Sort(vector<int>& nums, int low, int high){
+        if(low >= high){
+            return;
+        }
+        int mid = (low + high) / 2;
+        merge_Sort(nums, low, mid);
+        merge_Sort(nums, mid + 1, high);
+        merge(nums, low, mid, high);
+    }
+    vector<int> mergeSort(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        merge_Sort(nums, low, high);
+        return nums;
+    }
+};
+
+int main() {
+    vector<int> nums = {5, 2, 9, 1, 5, 6};
+    Solution sol;
+    vector<int> sorted = sol.mergeSort(nums);
+
+    cout << "Sorted array: ";
+    for(int num : sorted) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
