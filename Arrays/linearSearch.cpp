992c693c96f5fd08aed0858d5b58a06bd48c979a
RohanBisht33 <bishtrohan33@gmail.com>
2025-10-13 23:27:47 +0530

    linear search

---
+++ Arrays/linearSearch.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int linearSearch(vector<int>& nums, int target) {
+        //your code goes here
+        int count = 0;
+        for(int i = 0; i <nums.size(); i++){
+            if(nums[i] == target){
+                return count;
+            }
+            count++;
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {0,0,3,3,5,6,9,9};
+    int target = 5;
+
+    int ret = sol.linearSearch(nums,target);
+
+    cout << "\nReturned index: " << ret << "\n";
+    return 0;
+}
