145466baf23b34ebc7e1fafec17aa80f41d4c63c
RohanBisht33 <bishtrohan33@gmail.com>
2025-10-20 19:33:50 +0530

    rotating array left kth times

---
+++ Arrays/rotateLeft_kth.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void rotateArrayByOne(vector<int> &nums){
+        size_t n = nums.size();
+        if (n == 0)
+            return;
+
+        int head = nums[0];
+        for (size_t i = 0; i + 1 < n; ++i)
+        {
+            nums[i] = nums[i + 1];
+        }
+            nums[n - 1] = head;
+        }
+};
+int main()
+{
+    vector<int> a;
+
+    a = {1, 2, 3, 4, 5};
+
+    Solution sol;
+    sol.rotateArrayByOne(a);
+
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        cout << a[i];
+    }
+    cout << '\n';
+    return 0;
+}
