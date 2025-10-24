13dd7147f33d51b0fe89b746e5c2ac1cc161d89e
RohanBisht33 <bishtrohan33@gmail.com>
2025-10-24 23:58:24 +0530

    union of 2 sorted arrays

--- Arrays/union_sortedArrays.cpp
+++ Arrays/union_sortedArrays.cpp
@@ -1,50 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> unionSortedArrays(const vector<int>& a, const vector<int>& b) {
-    int i = 0, j = 0;
-    int n = (int)a.size(), m = (int)b.size();
-    vector<int> res;
-    while (i < n && j < m) {
-        if (a[i] == b[j]) {
-            if (res.empty() || res.back() != a[i]) res.push_back(a[i]);
-            int v = a[i];
-            while (i < n && a[i] == v) ++i;
-            while (j < m && b[j] == v) ++j;
-        } else if (a[i] < b[j]) {
-            if (res.empty() || res.back() != a[i]) res.push_back(a[i]);
-            int v = a[i];
-            while (i < n && a[i] == v) ++i;
-        } else {
-            if (res.empty() || res.back() != b[j]) res.push_back(b[j]);
-            int v = b[j];
-            while (j < m && b[j] == v) ++j;
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, vector<int> &nums2, vector<int> &arr)
+    {
+        int left = 0, right = 0;
+
+        while (left < nums1.size() && right < nums2.size())
+        {
+            if (nums1[left] == nums2[right])
+            {
+                if (arr.empty() || arr.back() != nums1[left])
+                    arr.push_back(nums1[left]);
+                left++;
+                right++;
+            }
+            else if (nums1[left] < nums2[right])
+            {
+                if (arr.empty() || arr.back() != nums1[left])
+                    arr.push_back(nums1[left]);
+                left++;
+            }
+            else
+            {
+                if (arr.empty() || arr.back() != nums2[right])
+                    arr.push_back(nums2[right]);
+                right++;
+            }
+        }
+
+        while (left < nums1.size())
+        {
+            if (arr.empty() || arr.back() != nums1[left])
+                arr.push_back(nums1[left]);
+            left++;
+        }
+        while (right < nums2.size())
+        {
+            if (arr.empty() || arr.back() != nums2[right])
+                arr.push_back(nums2[right]);
+            right++;
         }
     }
-    while (i < n) {
-        if (res.empty() || res.back() != a[i]) res.push_back(a[i]);
-        int v = a[i];
-        while (i < n && a[i] == v) ++i;
-    }
-    while (j < m) {
-        if (res.empty() || res.back() != b[j]) res.push_back(b[j]);
-        int v = b[j];
-        while (j < m && b[j] == v) ++j;
-    }
-    return res;
-}
 
-int main() {
-    vector<int> nums1 = {1, 2, 3, 4, 5};
-    vector<int> nums2 = {1, 2, 7};
-
-    vector<int> u = unionSortedArrays(nums1, nums2);
-
-    cout << "[";
-    for (size_t k = 0; k < u.size(); ++k) {
-        cout << u[k];
-        if (k + 1 < u.size()) cout << ", ";
+    vector<int> unionArray(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end()); // ensure sorted
+        sort(nums2.begin(), nums2.end()); // ensure sorted
+        vector<int> arr;
+        merge(nums1, nums2, arr);
+        return arr;
     }
-    cout << "]\n";
+};
+
+int main()
+{
+    vector<int> nums1(6), nums2(5);
+
+    for (int &x : nums1)
+        cin >> x;
+    for (int &x : nums2)
+        cin >> x;
+
+    Solution sol;
+    vector<int> arr = sol.unionArray(nums1, nums2);
+
+    for (auto &e : arr)
+        cout << e << " ";
+    cout << endl;
+
     return 0;
 }
