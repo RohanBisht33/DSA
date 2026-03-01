b9e519394e41a279f5224564c12e3410709135d5
RohanBisht33 <bishtrohan33@gmail.com>
2026-03-02 00:26:58 +0530

    kadane algorithm in O(n^2)

--- Arrays/Medium/Kadane_Algorithm.py
+++ Arrays/Medium/Kadane_Algorithm.py
@@ -1,3 +1,23 @@
+class Solution:
+    def kadanes(self, nums):
+        maximum = min(nums)
+        print("size", len(nums), "max", maximum, "array", nums)
+        for i in range(len(nums)):
+            print("i = ", i)
+            sum = 0
+            for j in range(i, len(nums)):
+                print("j = ", j)
+                sum += nums[j]
+                print("new sum = ", sum)
+                maximum = max(sum, maximum)
+                print("new max = ", maximum)
+
+        return maximum
+sol = Solution()
+print(sol.kadanes([-2, -3, -7, -2, -10, -4]))
+
+
+
 # class Solution:
 #     def kadanes(self, nums):
 #         maximum = min(nums)
@@ -17,5 +37,3 @@
 #         return maximum
 # sol = Solution()
 # print(sol.kadanes([-2, -3, -7, -2, -10, -4]))
-
-
