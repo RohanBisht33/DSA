9369bb5ad45dee19d721de42f9ed4ce8849dbcec
RohanBisht33 <bishtrohan33@gmail.com>
2026-03-02 00:18:57 +0530

    kadane's algorithm with brute force approach

---
+++ Arrays/Medium/Kadane_Algorithm.py
@@ -0,0 +1,21 @@
+# class Solution:
+#     def kadanes(self, nums):
+#         maximum = min(nums)
+#         print("size", len(nums), "max", maximum, "array", nums)
+#         for i in range(len(nums)):
+#             print("i = ", i)
+#             for j in range(i, len(nums)):
+#                 print("j = ", j)
+#                 sum = 0
+#                 for k in range(i, j+1):
+#                     print("k = ", k)
+#                     sum+= nums[k]
+#                     print("new sum = ", sum)
+#                 maximum = max(sum, maximum)
+#                 print("new max = ", maximum)
+
+#         return maximum
+# sol = Solution()
+# print(sol.kadanes([-2, -3, -7, -2, -10, -4]))
+
+
