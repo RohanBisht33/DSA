edcc1be4b905c434e59ea3827d6a6cc6b6701e5e
RohanBisht33 <bishtrohan33@gmail.com>
2026-03-08 00:00:28 +0530

    basic dsa template snippet

---
+++ Arrays/Medium/nextPermutation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextPermutation(self, array):
+        print("FAAAAAAAH")
+        return array
+
+nums = []
+t = int(input())
+while t:
+    nums.append(int(input()))
+    t -= 1
+
+sol = Solution()
+print(sol.nextPermutation(nums))
