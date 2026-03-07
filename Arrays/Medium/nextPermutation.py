c2226df5a587a0543a5b8d559caad835b10202f0
RohanBisht33 <bishtrohan33@gmail.com>
2026-03-08 01:29:52 +0530

    determining the next permutation of given array

--- Arrays/Medium/nextPermutation.py
+++ Arrays/Medium/nextPermutation.py
@@ -1,7 +1,24 @@
 class Solution:
-    def nextPermutation(self, array):
-        print("FAAAAAAAH")
-        return array
+    def nextPermutation(self, nums):
+        n = len(nums)
+
+        # Step 1: find breakpoint
+        i = n - 2 #second last element
+        while i >= 0 and nums[i] >= nums[i+1]: #while descending order, move to the range position
+            i -= 1
+
+        # Step 2: find next greater element
+        if i >= 0: 
+            j = n - 1
+            print(i,j)
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # Step 3: reverse suffix
+        nums[i+1:] = reversed(nums[i+1:])
+        
+        return nums
 
 nums = []
 t = int(input())
