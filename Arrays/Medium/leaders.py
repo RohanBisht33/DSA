8d355ae2ab3f930e1a2bc3dd44cdfd7b62777506
RohanBisht33 <bishtrohan33@gmail.com>
2026-03-09 20:10:07 +0530

    optimal method of finding leaders in an array

--- Arrays/Medium/leaders.py
+++ Arrays/Medium/leaders.py
@@ -5,20 +5,29 @@ class Solution:
         i = 0
         n = len(array)
         isLeader = False
+        max = n-1
 
-        for i in range(n):
-            if(i == n-1):
+        for i in range(n-1,-1,-1):
+            if(array[i] >= array[max]):
                 num.append(array[i])
-                break
-            for j in range(i+1, n):
-                if(array[j] > array[i]):
-                    isLeader = False
-                    break
-                else:
-                    isLeader = True
-            if(isLeader):
-                num.append(array[i])
-                
+                max = i
+            
+
+        # for i in range(n):
+        #     if(i == n-1):
+        #         num.append(array[i])
+        #         break
+        #     for j in range(i+1, n):
+        #         if(array[j] > array[i]):
+        #             isLeader = False
+        #             break
+        #         else:
+        #             isLeader = True
+        #     if(isLeader):
+        #         num.append(array[i])
+
+        
+        num = num[::-1]
         return num
 
 nums = []
