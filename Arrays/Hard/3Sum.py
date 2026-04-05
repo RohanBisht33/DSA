f3b64e0daee2d1c1819a38198c75f58ad6b80308
RohanBisht33 <bishtrohan33@gmail.com>
2026-04-05 23:52:02 +0530

    three sum using better approach

--- Arrays/Hard/3Sum.py
+++ Arrays/Hard/3Sum.py
@@ -3,22 +3,28 @@ class Solution:
     # Function to find triplets with sum zero
     def threeSum(self, arr, n):
         # Store unique triplets
-        st = set()
+        ans = set()
 
         # First loop for first element
         for i in range(n):
+            # Set to store elements seen in this iteration
+            hashset = set()
+
             # Second loop for second element
             for j in range(i + 1, n):
-                # Third loop for third element
-                for k in range(j + 1, n):
-                    # If triplet sum is zero
-                    if arr[i] + arr[j] + arr[k] == 0:
-                        # Store sorted triplet to avoid duplicates
-                        triplet = tuple(sorted([arr[i], arr[j], arr[k]]))
-                        st.add(triplet)
+                # Calculate third element needed
+                third = -(arr[i] + arr[j])
+
+                # If third already in set, we found a triplet
+                if third in hashset:
+                    triplet = tuple(sorted([arr[i], arr[j], third]))
+                    ans.add(triplet)
+
+                # Add current element to set
+                hashset.add(arr[j])
 
         # Convert set to list of lists
-        return [list(triplet) for triplet in st]
+        return [list(triplet) for triplet in ans]
 
 # Driver code
 if __name__ == "__main__":
